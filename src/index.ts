8b1c47682381766c5baf1c7197b60749689d75f9
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2020-12-10 19:57:42 -0800

    scan github

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 async function ls() {
-  const { stdout, stderr } = await exec('ls');
+  const { stdout, stderr } = await exec('ai-scan --url https://github.com/');
   console.log('stdout:', stdout);
   console.log('stderr:', stderr);
 }
