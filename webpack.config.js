071a31a54c8e0d4970e97d07f0f4ea6cee8b4845
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2020-12-17 15:14:53 -0800

    update externals3

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = (env) => {
     console.log(`Building for version : ${version}`);
     return {
         devtool: 'cheap-source-map',
-        externals: ['apify', 'apify-shared', 'puppeteer', 'yargs', 'levelup', 'leveldown'],
+        externals: ['levelup', 'leveldown'],
         entry: {
             ['index']: path.resolve('./src/index.ts'),
         },
