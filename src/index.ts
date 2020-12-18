cda8b6ec09942a2eefcd976afbdb5a723fa28557
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2020-12-17 20:08:57 -0800

    remove leveldown install

--- src/index.ts
+++ src/index.ts
@@ -8,24 +8,12 @@ import { Scanner } from './scanner/scanner';
 import { isEmpty } from 'lodash';
 
 (async () => {
-    var util = require('util');
-    var exec = util.promisify(require('child_process').exec);
-    const { stdout, stderr } = await exec(`npm install -g leveldown`);
+    const container = setupIocContainer();
+    const logger = container.get(Logger);
+    await logger.setup();
 
-    if (!isEmpty(stderr)) {
-        console.log("couldn't install external dependencies");
-        throw Error(stderr);
-    }
-
-    if (!isEmpty(stdout)) {
-        console.log(stdout);
-        const container = setupIocContainer();
-        const logger = container.get(Logger);
-        await logger.setup();
-
-        const scanner = container.get(Scanner);
-        await scanner.scan();
-    }
+    const scanner = container.get(Scanner);
+    await scanner.scan();
 })().catch((error) => {
     console.log('Exception thrown in action: ', error);
     process.exit(1);
