a7b39f725c14ac7df71e4c0d110698917c64da90
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2020-12-20 10:15:02 -0800

    add apify to externals

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = (env) => {
     console.log(`Building for version : ${version}`);
     return {
         devtool: 'cheap-source-map',
-        externals: ['leveldown'],
+        externals: ['leveldown', 'apify'],
         entry: {
             ['index']: path.resolve('./src/index.ts'),
         },
