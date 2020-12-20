f9fdd3d124238fb24e399e36ac8a3662304636f9
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2020-12-20 09:16:49 -0800

    remove leveldown and levelup from externals

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = (env) => {
     console.log(`Building for version : ${version}`);
     return {
         devtool: 'cheap-source-map',
-        externals: ['leveldown', 'levelup', 'puppeteer', 'axe-core', '@axe-core/puppeteer', 'apify', 'apify-shared'],
+        externals: ['puppeteer', 'axe-core', '@axe-core/puppeteer', 'apify', 'apify-shared'],
         entry: {
             ['index']: path.resolve('./src/index.ts'),
         },
