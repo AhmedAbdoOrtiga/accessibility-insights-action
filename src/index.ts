ad0f02616ff96edb3346d2149842a92c071ef5db
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2020-12-20 08:42:34 -0800

    add all externals

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,18 @@ import 'reflect-metadata';
 import { setupIocContainer } from './ioc/setup-ioc-container';
 import { Logger } from './logger/logger';
 import { Scanner } from './scanner/scanner';
-// import {isEmpty} from 'lodash';
+import { isEmpty } from 'lodash';
 
 (async () => {
-    var util = require('util');
-    var exec = util.promisify(require('child_process').exec);
-    const { stdout, stderr } = await exec(`npm install leveldown levelup puppeteer axe-core @axe-core/puppeteer apify apify-shared`);
-    
+    // var util = require('util');
+    // var exec = util.promisify(require('child_process').exec);
+    // const { stdout, stderr } = await exec(`npm install leveldown levelup puppeteer axe-core @axe-core/puppeteer apify apify-shared`);
+
+    // if (!isEmpty(stderr)) {
+    //     throw Error(stderr);
+    // }
+    // if (!isEmpty(stdout)) {
+    // console.log('All external packages were installed successfully!\n');
 
     const container = setupIocContainer();
     const logger = container.get(Logger);
@@ -19,7 +24,8 @@ import { Scanner } from './scanner/scanner';
 
     const scanner = container.get(Scanner);
     await scanner.scan();
-})().catch((error) => {
+    // }
+})().catch(error => {
     console.log('Exception thrown in action: ', error);
     process.exit(1);
 });
