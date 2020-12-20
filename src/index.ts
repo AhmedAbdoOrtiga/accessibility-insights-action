10fd0aa5f75c84a822c02b5d5439459c9b3eea67
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2020-12-20 08:24:11 -0800

    remove all npm modules

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,14 @@ import 'reflect-metadata';
 import { setupIocContainer } from './ioc/setup-ioc-container';
 import { Logger } from './logger/logger';
 import { Scanner } from './scanner/scanner';
+// import {isEmpty} from 'lodash';
 
 (async () => {
+    var util = require('util');
+    var exec = util.promisify(require('child_process').exec);
+    const { stdout, stderr } = await exec(`npm install leveldown levelup puppeteer axe-core @axe-core/puppeteer apify apify-shared`);
+    
+
     const container = setupIocContainer();
     const logger = container.get(Logger);
     await logger.setup();
